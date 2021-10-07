3f8e2176bc3ed2ae780a88ec063d09a0c62cf01a
Mohasin <mohasin61@gmail.com>
2021-10-07 15:26:19 +0530

    Add loveCalc.js

---
+++ javascript learning/loveCalc.js
@@ -0,0 +1,8 @@
+//program to calculate the love percent using Math.random()
+
+var boy = prompt("Enter your name:");
+var girl = prompt ("Enter your name:");
+
+let randomPercent = (Math.random()*100)+1;
+
+console.log(Math.floor(randomPercent)+"%");
