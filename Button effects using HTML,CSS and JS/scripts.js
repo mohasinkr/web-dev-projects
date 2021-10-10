e412866c397a4286396553317871cba86c6482ea
Mohasin <mohasin61@gmail.com>
2021-10-10 16:45:21 +0530

    change function name

--- Button effects using HTML,CSS and JS/scripts.js
+++ Button effects using HTML,CSS and JS/scripts.js
@@ -1,9 +1,7 @@
 var flag = true;
 var colors = ["#1DB9C3", "#7027A0", "#F56FAD", "#f5fdb0", "white","red"];
 
-const hoverEffects = (element, event) => {
-  var eventX = event.clientX;
-  var eventY = event.clientY;
+const colorSwitch = (element) => {
   let randomIndex = Math.floor(Math.random() * colors.length);
   // console.log(`${randomIndex} ${colors[randomIndex]}`);
   if (flag == true) {
