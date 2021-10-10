fadd4d3e268914239826720766738644f1395d18
Mohasin <mohasin61@gmail.com>
2021-10-10 16:40:34 +0530

    Rename folder, add random color picker from array

--- Ripple Button Effect using HTML,CSS and JS/scripts.js
+++
@@ -1,7 +0,0 @@
-const hoverEffects = (element,event) => {
-    var eventX = event.clientX;
-    var eventY = event.clientY;
-    element.style.backgroundColor = "red";
-    console.log(eventX + " " + eventY);
-  };
-  
