36161df5708c71f4badee3c9b53d59ea24bbfbfd
Mohasin <mohasin61@gmail.com>
2021-10-11 16:12:55 +0530

    Add objectDestructuring.js

---
+++ javascript programs/objectDestructuring.js
@@ -0,0 +1,8 @@
+const person = { name: "MKR", age: 21, sex: "MALE" };
+
+const { name, age, sex } = person;
+
+console.log(
+  `Name of the user : ${name}\n Age of the user: ${age} \n Sex of the user: ${sex}`
+);
+
