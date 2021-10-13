99813b6e829dd680ade0271bbc428a2c7fd8a35d
Mohasin <mohasin61@gmail.com>
2021-10-13 22:57:17 +0530

    Set text field to null afer clicking submit button

--- Todo List App using Vanilla Javascript/app.js
+++ Todo List App using Vanilla Javascript/app.js
@@ -7,32 +7,32 @@ todoButton.addEventListener("click", addTodo);
 function addTodo(event) {
   event.preventDefault(); //prevents the submit action ...
   console.log("hello!");
-
-  //creating a todo div inside ul todo-items class
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-
-  const newTodo = document.createElement("li");
-  newTodo.classList.add("todo-items");
-//   newTodo.innerHTML = "HEY";
-  todoDiv.appendChild(newTodo);
-
   const getText = todoInput.value;
-  newTodo.innerHTML = getText;
+  
+    //creating a todo div inside ul todo-items class
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
 
+    const newTodo = document.createElement("li");
+    newTodo.classList.add("todo-items");
+    //   newTodo.innerHTML = "HEY";
+    todoDiv.appendChild(newTodo);
 
-  //adding the task complete button
-  const completeButton = document.createElement("button");
-  completeButton.innerHTML = '<i class="fas fa-check"></i>';
-  completeButton.classList.add("complete-btn");
-  todoDiv.appendChild(completeButton);
+    newTodo.innerHTML = getText;
+    todoInput.value = "";
 
-  //adding the trash button...
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
+    //adding the task complete button
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
 
-  //adding to the main to the <ul> tag...
-  todoList.appendChild(todoDiv);
-}
+    //adding the trash button...
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //adding to the main to the <ul> tag...
+    todoList.appendChild(todoDiv);
+  }
