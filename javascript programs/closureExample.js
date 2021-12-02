1453a91be8e8134ee63245cf10780395457ce540
Mohasin <mohasin61@gmail.com>
2021-12-02 22:54:44 +0530

    Update closureExample.js

--- javascript programs/closureExample.js
+++ javascript programs/closureExample.js
@@ -12,3 +12,14 @@ const booker = secureBooking();
 booker();
 booker();
 booker();
+
+function adder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+const add10 = adder(5);
+const add20 = adder(10);
+console.log(add10(10));
+console.log(add20(20));
