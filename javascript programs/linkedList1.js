874c4c589f7c70eac247e4e6e8c735956dd41998
Mohasin <mohasin61@gmail.com>
2022-01-25 22:29:00 +0530

    Add linkedList1.js

---
+++ javascript programs/linkedList1.js
@@ -0,0 +1,15 @@
+const list = {
+  head: {
+    value: 10,
+    next: {
+      value: 20,
+      next: {
+        value: 30,
+        next: {
+          value: 40,
+          next: null,
+        },
+      },
+    },
+  },
+};
