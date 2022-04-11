a09219f005a08ee8312b77d65d88551950a7f309
Mohasin <mohasin61@gmail.com>
2022-04-11 20:41:35 +0530

    Update objectDestructuring.js

--- javascript programs/objectDestructuring.js
+++ javascript programs/objectDestructuring.js
@@ -1,8 +1,20 @@
 const person = { name: "MKR", age: 21, sex: "MALE" };
 
-const { name, age, sex } = person;
+const { name:myname, age:myage, sex:mysex } = person;
 
 console.log(
-  `Name of the user : ${name}\n Age of the user: ${age} \n Sex of the user: ${sex}`
+  `Name of the user : ${myname}\n Age of the user: ${myage} \n Sex of the user: ${mysex}`
 );
+console.log();
+
+//or
+
+const robot = { name: "robo007", age: 1, sex: "other" };
+
+const { name,age,sex} = robot;
+
+console.log(
+  `Name of the robo : ${name}\n Age of the robo: ${age} \n Sex of the robo: ${sex}`
+);
+
 
